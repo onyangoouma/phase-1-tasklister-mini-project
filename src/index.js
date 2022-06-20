23330dfdf7fb63fe1d057448b13297c45d31ffbf
onyangoouma <jacksonouma04@gmail.com>
2022-06-20 09:12:00 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
+  const form = document.getElementById('create-task-form');
+const inputValue = document.querySelector('#new-task-description')
+const todoList = document.getElementById('task')
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  form.addEventListener('submit', newTask);
+ 
+  // console.log(inputValue)
 });
+
+const newTask = (e) =>{
+  e.preventDefault();
+
+  let todoList = document.getElementById('new-task-description').value;
+  newAppendedTask(todoList)
+  e.target.reset()
+
+};
+
+const newAppendedTask = (task) =>{
+  let li = document.createElement('li')
+  li.textContent = task
+  document.getElementById('tasks').appendChild(li)
+}
